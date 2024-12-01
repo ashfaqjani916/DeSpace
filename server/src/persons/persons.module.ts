225063486b6f40eece8af54834899163ef6a4aad
Pradhyumna789 <hariharshasripradhyumna@gmail.com>
2024-12-02 04:17:44 +0530

    solved merge conflicts

--- server/src/persons/persons.module.ts
+++ server/src/persons/persons.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Person, PersonSchema } from './model';
@@ -9,3 +10,23 @@ import { Person, PersonSchema } from './model';
   exports: [MongooseModule], // Export MongooseModule to make it available to other modules
 })
 export class PersonModule { }
+=======
+// persons.module.ts
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PersonService } from './persons.service';
+import { Person, PersonSchema, Meeting, MeetingSchema, PersonList, PersonListSchema } from './model';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Person.name, schema: PersonSchema },
+      { name: Meeting.name, schema: MeetingSchema },
+      { name: PersonList.name, schema: PersonListSchema },
+    ]),
+  ],
+  providers: [PersonService],
+  exports: [PersonService], // Export the service to make it available to other modules
+})
+export class PersonsModule {}
+>>>>>>> abdf584 (resolved errors related to mongoose)
