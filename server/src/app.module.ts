a09412e65d4d11391509b0ce16a0f6fcb55197a1
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-12-02 03:49:24 +0530

    update changes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,11 +6,17 @@ import { SignalingModule } from './signaling/signaling.module';
 import { MediasoupService } from './mediasoup/mediasoup.service';
 import { MediasoupGateway } from './mediasoup/mediasoup.gateway';
 import { OktoApiModule } from './okto-api/okto-api.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PersonsService } from './persons/persons.service';
 
 @Module({
-  imports: [ChatModule, SignalingModule, OktoApiModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://lokeshnagasaid:vz8bsAYmp2q5hg5r@undefined/?replicaSet=atlas-pyn6b1-shard-0&ssl=true&authSource=admin', {
+
+    }),
+    ChatModule, SignalingModule, OktoApiModule],
   controllers: [AppController],
-  providers: [AppService, MediasoupService, MediasoupGateway],
+  providers: [AppService, MediasoupService, MediasoupGateway, PersonsService],
 })
 export class AppModule { }
 
