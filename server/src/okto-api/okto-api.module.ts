a9a2ea8d1963870f012435c2fc4185d159606eb4
Pradyumna <hariharshasripradhyumna@gmail.com>
2024-12-02 04:54:50 +0530

    Revert "solved merge conflicts"
    This reverts commit 225063486b6f40eece8af54834899163ef6a4aad.

--- server/src/okto-api/okto-api.module.ts
+++ server/src/okto-api/okto-api.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OktoApiService } from './okto-api.service';
 import { OktoApiController } from './okto-api.controller';
-<<<<<<< HEAD
 import { PersonService } from 'src/persons/persons.service';
 
 @Module({
   providers: [OktoApiService, PersonService],
-=======
-import { PersonsModule } from 'src/persons/persons.module';
-
-@Module({
-  providers: [OktoApiService, PersonsModule],
->>>>>>> abdf584 (resolved errors related to mongoose)
   controllers: [OktoApiController]
 })
 export class OktoApiModule { }
